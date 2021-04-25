96d95a3881815216270852a0d0a054404ae1425c
Ilannildo <ilannildoviana12@gmail.com>
2021-04-25 18:49:15 -0300

    Alterado a baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.10.105:3333',
+  baseURL: 'https://my-json-server.typicode.com/Ilannildo/plantmanager',
 });
 
 export default api;
